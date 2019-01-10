995a9cd833da0f1e14e4b1d76d321be954b5963f
Joakim Svendsen <joakimbs@gmail.com>
2019-01-10 04:07:02 +0100

    Fix docs

--- SnP500/SnP500Trend.ps1
+++ SnP500/SnP500Trend.ps1
@@ -88,7 +88,7 @@ function Get-SnP500Trend {
             In addition to this, I included parameters for great flexibility in the data set used
             to calculate, except (at least) one flaw, in that I only focus on the *latest* measurements, so
             this tool is currently not suited for "retro-analysis", meaning looking at periods 
-            back in time, and omitting the present, rather than predicting from, say January 1982
+            back in time, and omitting the present, predicting from, say January 1982
             to February 1982, to see if it matches. The assumed need for this is in less demand
             than the present becoming future, since the latter is where life takes place, but it's
             on the to do list.
